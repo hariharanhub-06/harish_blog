bee69dfeaec127e2277f6d3bd990b2b9b18c8da8
Hari Haran <hariharanjeyaramamoorthy@gmail.com>
2026-02-26 10:37:10 +0530

    Remove Agile Scrum module and associated database tables

---
+++ drizzle/0008_fresh_metal_master.sql
@@ -0,0 +1,7 @@
+DROP TABLE "agile_deployment_logs" CASCADE;--> statement-breakpoint
+DROP TABLE "agile_epics" CASCADE;--> statement-breakpoint
+DROP TABLE "agile_issues" CASCADE;--> statement-breakpoint
+DROP TABLE "agile_meetings" CASCADE;--> statement-breakpoint
+DROP TABLE "agile_projects" CASCADE;--> statement-breakpoint
+DROP TABLE "agile_sprints" CASCADE;--> statement-breakpoint
+DROP TABLE "agile_workflows" CASCADE;
